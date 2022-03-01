f86187a3ff2b9017112b91f7a4634e1220ffa882
walk8243 <walk8243@gmail.com>
2022-03-02 01:39:11 +0900

    コマンドの追加

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,27 @@
 import { Command } from 'commander';
+import { spawn } from 'child_process';
 const program = new Command();
 
 program
 	.name('@walk8243/app')
 	.description('Node.jsを使ってCLIアプリケーションの作成')
 	.version('1.0.0', '-V, --version', 'バージョンを表示する')
-	.helpOption('-h, --help', 'ヘルプを表示する')
+	.helpOption('-h, --help', 'ヘルプを表示する');
+
+program
 	.option('--first', '最初の区切りのみを表示する')
 	.option('-d, --debug', 'デバッグモードで実行する')
 	.option('-s, --separator <char>', 'コマンドライン引数の区切り文字を指定する', ',')
-	.parse();
+	.action((options) => {
+		console.log('options:', options);
+		const limit = options.first ? 1 : undefined;
+		console.log('arg:', program.args.map((arg) => arg.split(options.separator, limit)));
+	});
 
-const options = program.opts();
-console.log('options:', options);
-const limit = options.first ? 1 : undefined;
-console.log('arg:', program.args.map((arg) => arg.split(options.separator, limit)));
+program.command('graph')
+	.action(() => {
+		const git = spawn('git', [ 'log', '--graph', '--date=short', '--pretty=%C(yellow)%h %C(cyan)%ad %C(green)%an%Creset%x09%s %C(red)%d%Creset' ], { stdio: 'inherit' });
+		git.on('exit', () => {});
+	});
+
+program.parse();
